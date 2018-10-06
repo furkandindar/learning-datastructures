ff8f5945805e40acd5a5ee0f8a6e68ee17d62aa6
furkandindar <fdindar1@binghamton.edu>
2018-10-06 00:04:33 -0400

    Test commit

--- src/ds/stack/App.java
+++ src/ds/stack/App.java
@@ -3,7 +3,7 @@ package ds.stack;
 public class App {
 
     public static void main(String[] args) {
-        Stack s = new Stack(3);
+        Stack s = new Stack(3);// test commit
         s.push(1);
         s.push(3);
         s.push(5);
